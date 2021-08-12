ff04523365d665c1e52c9ee50f53660d5c784954
nbn <nbn1209@126.com>
2021-08-12 23:37:13 +0800

    feat: submit

---
+++ assets/index.73941145.js
@@ -0,0 +1,1 @@
+import{d as a,o as e,c as s,p as t}from"./vendor.fbc6625e.js";var d=a({setup:()=>({})});const o=t()(((a,t,d,o,r,c)=>(e(),s("div",null,"test"))));d.render=o,d.__scopeId="data-v-472ac4ac";export default d;
